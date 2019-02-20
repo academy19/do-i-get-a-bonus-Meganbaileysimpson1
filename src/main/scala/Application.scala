5b0f8600f25e0d75924e59c116dd1f6d82ba322a
Megan Bailey-Simpson <megan.baileysimpson@digital.hmrc.gov.uk>
2019-02-20 10:55:18 +0000

    bonus

--- src/main/scala/Application.scala
+++ src/main/scala/Application.scala
@@ -2,10 +2,19 @@ object Application {
 
   // write your function here
 
+  def calculateBonus (income: Int, bonus: Boolean): String = {
+    if (bonus == true) {
+      val wage:Int= (income * 1.2).toInt
+      s"£$wage"
+    }
+
+    else s"£$income"
+  }
+
 
   def main(args : Array[String]) : Unit = {
     println(calculateBonus(100, true))  // should print £120
     println(calculateBonus(100, false)) // should print £100
-    println(calculateBonus(99, true))   // should print £118
-  }
+  println(calculateBonus(99, true))   // should print £118
+}
 }
